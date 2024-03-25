73eb499813d78c7789d8a46fbd240a8916dbb763
ForasterXD <sebastiandlqs@gmail.com>
2024-03-24 20:56:55 -0400

    A proposal for the index page of how to show the dinosaurs

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,22 @@
 import data from './json.js'
 
+const dinoIndex = document.getElementById('dino-index')
 
-console.log(data);
+function newDinosaurInIndex(name, imageSrc, foundIn, diet){
+    let newDino = document.createElement('li')
+    newDino.classList.add('dino')
+
+    newDino.innerHTML = 
+    `
+    <img class="background-image" src="${imageSrc}" alt="${name}" />
+    <a href="./dinosaur-details.html">
+        <h1 class="dino-name">${name}</h1>
+    </a>
+    `
+
+    dinoIndex.children[1].appendChild(newDino)
+}
+
+data.forEach(dino => {
+    newDinosaurInIndex(dino.name, dino.imageSrc, dino.foundIn, dino.diet)
+});
