5b37f39d8216572bf982f610747676219beb7163
Jordan Osikowicz <jordan9644@gmail.com>
2024-03-26 19:41:15 -0400

    render dinosaur list

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,9 @@
 import data from './json.js'
+const anchor = document.getElementById('dino-list')
 
-
-console.log(data);
+data.map(dinosaur => {
+    const li = document.createElement('li')
+    li.innerHTML = dinosaur.name
+anchor.append(li)
+return "list item added of " + dinosaur.name + " to the list"
+});
