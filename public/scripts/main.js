0690d5e68a4465adf1ec084867026f2deeab9dc0
Mikey Nichols <mikenichols410@gmail.com>
2024-03-26 16:23:44 -0400

    adds some temp code for testing the display (data) dinosaur feat

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,54 +1,14 @@
-import data from "./json.js";
-import createElement from "./utils/createEle.js";
+import data from './json.js'
+import displayDinosaur from './components/details.view.js';
 
-const displayDinosaur = (dino, anchor = document.createElement("div")) => {
-  const {
-    description,
-    diet,
-    foundIn,
-    id,
-    imageSrc,
-    length,
-    name,
-    namedBy,
-    taxonomy,
-    typeOfDinosaur,
-    typeSpecies,
-    weight,
-    whenLived,
-  } = dino;
+console.log(data);
 
+// temporary testing (This is a temporary workaround to allow me to test the display data feature -mn)
 
-  anchor.id = id;
-  anchor.className = "modal-container";
-  const content = `
-    <div class="header-flex">
-    <h1 class="dinosaur-name">${name} (${taxonomy})</h1>
-    <p><strong>Named By:</strong> ${namedBy}</p>
-    <span class="close-btn">&times;</span>
-    </div>
-    <div class="dinosaur-image-container">
-    <img class="dinosaur-detail-image"
-      src="${imageSrc}"
-      alt="Artwork reperesenting a ${name}}" />
-    </div>
-    <div class="container">
-    <p><strong>Found In:</strong> ${foundIn}</p>
-    <p><strong>Type:</strong> ${typeOfDinosaur}</p>
-    <p><strong>Length:</strong> ${length}/p>
-    <p><strong>Weight:</strong> ${weight}</p>
-    <p><strong>Diet:</strong> ${diet}</p>
-    <p><strong>Lived during:</strong> ${whenLived}</p>
-    <p><strong>Species Type:</strong> ${typeSpecies}</p>
-    <p><strong>Description:</strong> ${description}</p>   
-    </div>
-    `;
-  createElement("div", content, anchor);
-  document.body.append(anchor);
-  const modal = document.querySelector(`.modal-container`);
-  const closeBtn = modal.querySelector(".close-btn");
-  closeBtn.style = "cursor:pointer";
-  closeBtn.onclick = () => modal.remove();
-};
-
-displayDinosaur(data[1]);
+const modalAnchor = document.createElement("div"); // temporary var for until I know what container that Sebastian creates
+Array.from(document.querySelectorAll('li.dino')).forEach((dino, index) => {
+    dino.style = "cursor: pointer" // changes the cursor to pointer
+    dino.onclick = e => {
+        displayDinosaur(data[index], modalAnchor); // adds an event listener to every dinosaur and onclick we call the displayDinosaur function, passing in the data of the dinosaur we clicked on, and the anchor of the modal.
+    }
+})
