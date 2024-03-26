e8835981a024c5fb6b7efc8a6ad5638a0aaa1000
Mikey Nichols <mikenichols410@gmail.com>
2024-03-26 15:53:14 -0400

    modiefies createElement to return the element that it creates to allow chaining

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,54 @@
-import data from './json.js'
+import data from "./json.js";
+import createElement from "./utils/createEle.js";
+
+const displayDinosaur = (dino, anchor = document.createElement("div")) => {
+  const {
+    description,
+    diet,
+    foundIn,
+    id,
+    imageSrc,
+    length,
+    name,
+    namedBy,
+    taxonomy,
+    typeOfDinosaur,
+    typeSpecies,
+    weight,
+    whenLived,
+  } = dino;
 
 
-console.log(data);
+  anchor.id = id;
+  anchor.className = "modal-container";
+  const content = `
+    <div class="header-flex">
+    <h1 class="dinosaur-name">${name} (${taxonomy})</h1>
+    <p><strong>Named By:</strong> ${namedBy}</p>
+    <span class="close-btn">&times;</span>
+    </div>
+    <div class="dinosaur-image-container">
+    <img class="dinosaur-detail-image"
+      src="${imageSrc}"
+      alt="Artwork reperesenting a ${name}}" />
+    </div>
+    <div class="container">
+    <p><strong>Found In:</strong> ${foundIn}</p>
+    <p><strong>Type:</strong> ${typeOfDinosaur}</p>
+    <p><strong>Length:</strong> ${length}/p>
+    <p><strong>Weight:</strong> ${weight}</p>
+    <p><strong>Diet:</strong> ${diet}</p>
+    <p><strong>Lived during:</strong> ${whenLived}</p>
+    <p><strong>Species Type:</strong> ${typeSpecies}</p>
+    <p><strong>Description:</strong> ${description}</p>   
+    </div>
+    `;
+  createElement("div", content, anchor);
+  document.body.append(anchor);
+  const modal = document.querySelector(`.modal-container`);
+  const closeBtn = modal.querySelector(".close-btn");
+  closeBtn.style = "cursor:pointer";
+  closeBtn.onclick = () => modal.remove();
+};
+
+displayDinosaur(data[1]);
--- public/scripts/utils/createEle.js
+++ public/scripts/utils/createEle.js
@@ -2,6 +2,7 @@ const createElement = (tag, innerHTML, anchor) => {
     const ele = document.createElement(tag);
     ele.innerHTML = innerHTML;
     anchor.append(ele);
+    return ele;
 }
 
 export default createElement;
