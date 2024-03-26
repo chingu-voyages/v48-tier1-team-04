624e99f109ef2653eb7863d2cd941b6b15f4118f
Mikey Nichols <mikenichols410@gmail.com>
2024-03-25 21:37:06 -0400

    pushes code used in example during meeting

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,25 @@ import data from './json.js'
 
 const dinoIndex = document.getElementById('dino-index')
 
-function newDinosaurInIndex(name, imageSrc, foundIn, diet){
+function newDinosaurInIndex({id, name, imageSrc, foundIn, diet}){
     let newDino = document.createElement('li')
     newDino.classList.add('dino')
 
     newDino.innerHTML = 
     `
     <img class="background-image" src="${imageSrc}" alt="${name}" />
-    <a href="./dinosaur-details.html">
+    <a id=${id}>
         <h1 class="dino-name">${name}</h1>
     </a>
     `
-
+    
     dinoIndex.children[1].appendChild(newDino)
+
+    
 }
 
 data.forEach(dino => {
-    newDinosaurInIndex(dino.name, dino.imageSrc, dino.foundIn, dino.diet)
+    newDinosaurInIndex(dino)
+    document.getElementById(dino.id).onclick = () => console.log(dino)
 });
+
