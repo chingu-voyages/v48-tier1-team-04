76429310c14c29ecd3a3702bdfba7f50dad5e584
Mikey Nichols <mikenichols410@gmail.com>
2024-03-26 20:02:40 -0400

    resolves merge conflict and enables functionality with dynamic list

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -10,10 +10,10 @@ anchor.append(li)
 return "list item added of " + dinosaur.name + " to the list"
 });
 
-// temporary testing (This is a temporary workaround to allow me to test the display data feature -mn)
 
+// temporary testing (This is a temporary workaround to allow me to test the display data feature -mn)
 const modalAnchor = document.createElement("div"); // temporary var for until I know what container that Sebastian creates
-Array.from(document.querySelectorAll('li.dino')).forEach((dino, index) => {
+Array.from(document.querySelectorAll('li')).forEach((dino, index) => {
     dino.style = "cursor: pointer" // changes the cursor to pointer
     dino.onclick = e => {
         displayDinosaur(data[index], modalAnchor); // adds an event listener to every dinosaur and onclick we call the displayDinosaur function, passing in the data of the dinosaur we clicked on, and the anchor of the modal.
