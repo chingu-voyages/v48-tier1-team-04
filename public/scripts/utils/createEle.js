9d2e792224221c273d7566f6e1867cc87bc534ef
Mikey Nichols <mikenichols410@gmail.com>
2024-03-29 22:22:45 -0400

    renames createElement to createEle to mitigate confusion between document.createElement

--- public/scripts/utils/createEle.js
+++ public/scripts/utils/createEle.js
@@ -1,8 +1,8 @@
-const createElement = (tag, innerHTML, anchor) => {
+const createEle = (tag, innerHTML, anchor) => {
     const ele = document.createElement(tag);
     ele.innerHTML = innerHTML;
     anchor.append(ele);
     return ele;
 }
 
-export default createElement;
+export default createEle;
