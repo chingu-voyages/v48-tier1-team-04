ee4b11fa709a028258e23c0ff0fbc11d6c7f251b
Mikey Nichols <mikenichols410@gmail.com>
2024-03-29 22:55:16 -0400

    adds a console.log outside of dinoListItem to demonstrate the return value

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,4 @@
 import data from '../assets/dinosaurs.json' with { type: 'json'};
 import dinoListItem from "./components/dinoListItem.js";
 
-data.forEach((dinosaur) => dinoListItem(dinosaur)); // loop over the json file and call dinoListItem, passing inthe dinosaur each time resulting in a filled list of dinosaurs on screen
+data.forEach((dinosaur) => console.log(dinoListItem(dinosaur))); // loop over the json file and call dinoListItem, passing inthe dinosaur each time resulting in a filled list of dinosaurs on screen
