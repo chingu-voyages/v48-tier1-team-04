a1993e03065dee89243196792f1a02d4b6479d6c
Mikey Nichols <mikenichols410@gmail.com>
2024-03-29 22:36:41 -0400

    refactors map to forEach for added performance also added a comment

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,4 @@
 import data from '../assets/dinosaurs.json' assert { type: 'json'};
 import dinoListItem from "./components/dinoListItem.js";
 
-data.map((dinosaur) => dinoListItem(dinosaur));
+data.forEach((dinosaur) => dinoListItem(dinosaur)); // loop over the json file and call dinoListItem, passing inthe dinosaur each time resulting in a filled list of dinosaurs on screen
