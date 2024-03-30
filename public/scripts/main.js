3ab1d966c5b58a95e3f54f37348939625d8dac9f
ForasterXD <sebastiandlqs@gmail.com>
2024-03-30 13:04:39 -0400

    search functionality done

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,8 +5,7 @@ import filterDinosaursByName from "./utils/filterDinosaurs.js";
 data.forEach((dinosaur) => console.log(dinoListItem(dinosaur))); // loop over the json file and call dinoListItem, passing inthe dinosaur each time resulting in a filled list of dinosaurs on screen
 
 const searchBar = document.getElementById('search-bar')
-searchBar.addEventListener('input', () => {
-    //Here comes the displaying of the filtered dinosaurs
-    //an array with the dinosaurs filtered is on the function filterDinosaursByName
-    console.log(filterDinosaursByName(searchBar.value));
+searchBar.addEventListener('keyup', () => {
+    document.getElementById('dino-list').innerHTML = ''
+    filterDinosaursByName(searchBar.value).forEach((dinosaur) => console.log(dinoListItem(dinosaur)));
 })
--- public/scripts/utils/filterDinosaurs.js
+++ public/scripts/utils/filterDinosaurs.js
@@ -2,7 +2,7 @@ import data from '../../assets/dinosaurs.json' assert { type: 'json'};
 
 function filterDinosaursByName(query){
     return data.filter(dino => {
-        return dino.name.toLowerCase().indexOf(query.toLowerCase()) > -1
+        return dino.name.toLowerCase().includes(query.toLowerCase())
     })
 }
 
