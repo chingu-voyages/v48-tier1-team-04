f4c56c6cc9541b9ff6844811694a329512787cfc
Mikey Nichols <mikenichols410@gmail.com>
2024-03-29 22:22:24 -0400

    adds comments to dinoListItem

--- public/scripts/components/dinoListItem.js
+++ public/scripts/components/dinoListItem.js
@@ -1,8 +1,8 @@
-import createElement from "../utils/createEle.js";
-import displayDinosaur from "./detailsView.js";
+import createEle from "../utils/createEle.js";
+import displayDinosaur from "./displayDinosaur.js";
 
 const dinoListItem = (dinosaur) => {
-  const { diet, foundIn, imageSrc, name, typeSpecies, weight } = dinosaur;
+  const { diet, foundIn, imageSrc, name, typeSpecies, weight } = dinosaur; // destructures the dinosaur object into smaller chunks
   const content = `<li class="dino">
   <div>
     <img
@@ -28,12 +28,11 @@ const dinoListItem = (dinosaur) => {
     </div>`
         : " "
     }
-</li>`;
-  const anchor = document.getElementById('dino-list')
-  const dinoLi = createElement("li", content, anchor);
-  dinoLi.style = "cursor:pointer";
-  dinoLi.onclick = () => displayDinosaur(dinosaur);
-  return "list item added of " + name + " to the list";
+</li>`; // dynamically generarte the html for a single dinosaur list item
+  const parentContainer = document.getElementById('dino-list'); // declares the parent container which is the list of dinosaurs
+  const dinoLi = createEle("li", content, parentContainer); // declares a variable to a new `<li>` with the content of `content` and appended  to the parentContainer
+  dinoLi.onclick = () => displayDinosaur(dinosaur); // adds event listen on click to the list item just created, and calls the `displayDinosaur`, passing in this dinosaur into the function
+  return "Dinosaur added of " + name + " to the list"; // returns a log if we wanted to console.log our results
 };
 
 export default dinoListItem;
