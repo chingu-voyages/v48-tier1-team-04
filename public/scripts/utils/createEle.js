dc2cc92336b34144d96c1f78b22ab0c4ba1c6821
sebastiandlq <63024668+sebastiandlq@users.noreply.github.com>
2024-04-03 14:51:33 -0400

    Delete public directory for Vite compatibility
    Reviewed:
    @sebastiandlq 
    @vinccodes

--- public/scripts/utils/createEle.js
+++
@@ -1,8 +0,0 @@
-const createEle = (tag, innerHTML, parentContainer) => {
-    const ele = document.createElement(tag);
-    ele.innerHTML = innerHTML;
-    parentContainer.append(ele);
-    return ele;
-}
-
-export default createEle;
--- public/scripts/utils/filterDinosaurs.js
+++
@@ -1,9 +0,0 @@
-import data from '../../assets/dinosaurs.json' assert { type: 'json'};
-
-function filterDinosaursByName(query){
-    return data.filter(dino => {
-        return dino.name.toLowerCase().includes(query.toLowerCase())
-    })
-}
-
-export default filterDinosaursByName
