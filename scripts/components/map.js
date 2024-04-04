5f61c5f8aed135487d36b1d877c08681b6d2f9fb
Mikey Nichols <mikenichols410@gmail.com>
2024-04-03 20:40:44 -0400

    place a marker somewhere on the map

--- scripts/components/map.js
+++ scripts/components/map.js
@@ -1,17 +1,20 @@
 import mapboxgl from "mapbox-gl";
-import 'mapbox-gl/dist/mapbox-gl.css';
+import "mapbox-gl/dist/mapbox-gl.css";
 import MapboxGeocoder from "mapbox-gl-geocoder";
 
-mapboxgl.accessToken = import.meta.env.VITE_MAPBOXAPIKEY // The API key is stored within `.env` which is not tracked by git therefore you must obtain this information or place your own API key to mapbox in your own .env file using VITE_MAPBOXAPIKEY=<enter api key>
-
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOXAPIKEY; // The API key is stored within `.env` which is not tracked by git therefore you must obtain this information or place your own API key to mapbox in your own .env file using VITE_MAPBOXAPIKEY=<enter api key>
 
 const mapComponent = (data) => {
-    const map = new mapboxgl.Map({
-        container: "map",
-        style: "",
-        center: [0, 0],
-        zoom: 0
-    })
-}
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "",
+    center: [0, 0],
+    zoom: 0,
+  });
+  
+  const marker = new mapboxgl.Marker()
+  .setLngLat([0, 0]) 
+  .addTo(map);
+};
 
-export default mapComponent
+export default mapComponent;
