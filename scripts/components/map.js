acd4ce6ddbb03929a2c6d7bc257ec0efdd674e54
Mikey Nichols <mikenichols410@gmail.com>
2024-04-03 20:10:21 -0400

    initalize the map on screen

--- scripts/components/map.js
+++ scripts/components/map.js
@@ -2,12 +2,16 @@ import mapboxgl from "mapbox-gl";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapboxGeocoder from "mapbox-gl-geocoder";
 
-mapboxgl.accessToken = import.meta.env.VITE_MAPBOXAPIKEY
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOXAPIKEY // The API key is stored within `.env` which is not tracked by git therefore you must obtain this information or place your own API key to mapbox in your own .env file using VITE_MAPBOXAPIKEY=<enter api key>
 
 
-
-const map = () => {
-    console.log(mapboxgl.accessToken)
+const mapComponent = () => {
+    const map = new mapboxgl.Map({
+        container: "map",
+        style: "",
+        center: [0, 0],
+        zoom: 0
+    })
 }
 
-export default map
+export default mapComponent
