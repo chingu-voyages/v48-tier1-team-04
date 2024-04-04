00c497a5552b77b9c80c673f43fbeb13e0420787
Mikey Nichols <mikenichols410@gmail.com>
2024-04-03 23:45:29 -0400

    imports displayDinosaur into map and adds on click event to each marker to call it passing in the dinosaur

--- scripts/components/map.js
+++ scripts/components/map.js
@@ -1,5 +1,6 @@
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import displayDinosaur from "./displayDinosaur";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOXAPIKEY; // The API key is stored within `.env` which is not tracked by git therefore you must obtain this information or place your own API key to mapbox in your own .env file using VITE_MAPBOXAPIKEY=<enter api key>
 
@@ -15,17 +16,17 @@ const displayTeamLocations = (map, image) => {
   teamLocations.forEach((location) => fetchLocation(map, location, image));
 };
 
-const fetchLocation = async (map, query, image) => {
+const fetchLocation = async (map, query, image, dinosaur) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${mapboxgl.accessToken}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const coordinates = data.features[0].geometry.coordinates;
-      placeMarker(map, coordinates, image);
+      placeMarker(map, coordinates, image, dinosaur);
     });
 };
 
-const placeMarker = (map, coordinates, image) => {
+const placeMarker = (map, coordinates, image, dinosaur) => {
   const div = document.createElement("div");
   div.style.width = "50px";
   div.style.height = "50px";
@@ -35,10 +36,21 @@ const placeMarker = (map, coordinates, image) => {
     const imagesArr = ["dino-icon-1.png", "dino-icon-2.png"];
     const randomImg = imagesArr[Math.floor(Math.random() * imagesArr.length)];
     div.style.backgroundImage = `url(./assets/${randomImg})`;
+    div.style.cursor = "pointer";
+    div.onclick = () => displayDinosaur(dinosaur)
   }
   new mapboxgl.Marker(div).setLngLat(coordinates).addTo(map);
 };
 
+export const flyToLocation = (map, coordinates) => {
+  window.scrollTo(0, 0);
+  map.flyTo({
+    center: coordinates,
+    zoom: 10,
+    essential: true,
+  });
+
+}
 const mapComponent = (data) => {
   const map = new mapboxgl.Map({
     container: "map",
@@ -48,7 +60,7 @@ const mapComponent = (data) => {
   });
 
   displayTeamLocations(map, "url(./assets/chinguheart.png)");
-  data.forEach((dinosaur) => fetchLocation(map, dinosaur.foundIn));
+  data.forEach((dinosaur) => fetchLocation(map, dinosaur.foundIn, null, dinosaur));
 };
 
 export default mapComponent;
