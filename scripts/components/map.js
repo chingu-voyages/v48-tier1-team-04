d870f978baaa5726c9cfb941e169b0ce784daef2
Mikey Nichols <mikenichols410@gmail.com>
2024-04-03 21:58:33 -0400

    creates a conditional that will allow us to change the pin image by giving one but defaults to a chingu heart

--- scripts/components/map.js
+++ scripts/components/map.js
@@ -4,9 +4,16 @@ import "mapbox-gl/dist/mapbox-gl.css";
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOXAPIKEY; // The API key is stored within `.env` which is not tracked by git therefore you must obtain this information or place your own API key to mapbox in your own .env file using VITE_MAPBOXAPIKEY=<enter api key>
 
 const displayTeamLocations = (map) => {
-  const teamLocations = ["Bolivia", "Cincinnati", "Chile", "Gormania", "Pittsburgh", "Toronto"]
-  teamLocations.forEach(location => fetchLocation(map, location))
-}
+  const teamLocations = [
+    "Bolivia",
+    "Cincinnati",
+    "Chile",
+    "Gormania",
+    "Pittsburgh",
+    "Toronto",
+  ];
+  teamLocations.forEach((location) => fetchLocation(map, location));
+};
 
 const fetchLocation = async (map, query) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${mapboxgl.accessToken}`;
@@ -19,11 +26,12 @@ const fetchLocation = async (map, query) => {
     });
 };
 
-const placeMarker = (map, coordinates) => {
+const placeMarker = (map, coordinates, image) => {
   const div = document.createElement("div");
-  div.style.backgroundImage = "url(./assets/chinguheart.png)";
   div.style.width = "50px";
   div.style.height = "50px";
+  if (image) div.style.backgroundImage = image
+  else div.style.backgroundImage = "url(./assets/chinguheart.png)";
   new mapboxgl.Marker(div).setLngLat(coordinates).addTo(map);
 };
 
