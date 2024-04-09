0a4d5dd21a4c8bb6e612d643592fee78aa3f1853
Mikey Nichols <mikenichols410@gmail.com>
2024-04-09 15:36:07 -0400

    creates a nav component

--- scripts/components/nav.js
+++ scripts/components/nav.js
@@ -1,1 +1,26 @@
 import createEle from "../utils/createEle";
+const content = `
+<input type="checkbox" class="navigation__checkbox" id="navi-toggle">
+<label for="navi-toggle" class="navigation__button">
+  <span class="navigation__icon">&nbsp;</span>
+</label>
+<div class="navigation__background">&nbsp;</div>
+<nav class="navigation__nav">
+  <ul class="navigation__list">
+    <li class="navigation__item">
+      <a href="#" class="navigation__link">Dig Up Some Dinosaurs</a>
+    </li>
+    <li class="navigation__item">
+      <a href="#" class="navigation__link">View Some Charts</a>
+    </li>
+    <li class="navigation__item">
+      <a href="#" class="navigation__link">Learn More about Chingu</a>
+    </li>
+    <li class="navigation__item">
+      <a href="#" class="navigation__link">About The Chingu Raptors</a>
+    </li>
+  </ul>
+</nav>
+`;
+const renderNav = () => createEle("div", content, document.body, "navigation");
+export default renderNav;
