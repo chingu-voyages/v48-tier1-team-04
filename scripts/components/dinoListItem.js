e0d32dbc775554019fae8e3e082cd515c87e2f7e
Mikey Nichols <mikenichols410@gmail.com>
2024-04-09 14:16:15 -0400

    removes console.log

--- scripts/components/dinoListItem.js
+++ scripts/components/dinoListItem.js
@@ -31,7 +31,7 @@ const dinoListItem = (dinosaur) => {
 </li>`; // dynamically generarte the html for a single dinosaur list item
   const parentContainer = document.getElementById('dino-list'); // declares the parent container which is the list of dinosaurs
   const dinoLi = createEle("li", content, parentContainer); // declares a variable to a new `<li>` with the content of `content` and appended  to the parentContainer
-  dinoLi.onclick = () => console.log(displayDinosaur(dinosaur)); // adds event listen on click to the list item just created, and calls the `displayDinosaur`, passing in this dinosaur into the function
+  dinoLi.onclick = () => displayDinosaur(dinosaur); // adds event listen on click to the list item just created, and calls the `displayDinosaur`, passing in this dinosaur into the function
   return "Dinosaur added of " + name + " to the list"; // returns a log if we wanted to console.log our results
 };
 
