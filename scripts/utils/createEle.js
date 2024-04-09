ceb616ee11273afa329230f9358cd8851942ecc0
Mikey Nichols <mikenichols410@gmail.com>
2024-04-09 14:16:34 -0400

    adds conditional to prepend on demand

--- scripts/utils/createEle.js
+++ scripts/utils/createEle.js
@@ -1,9 +1,9 @@
-const createEle = (tag, innerHTML, parentContainer, classList, id) => {
+const createEle = (tag, innerHTML, parentContainer, classList, id, pre) => {
     const ele = document.createElement(tag);
     ele.innerHTML = innerHTML;
     classList ? ele.classList = classList : null
     id ? ele.id = id : null;
-    parentContainer.append(ele);
+    pre ? parentContainer.prepend(ele) : parentContainer.append(ele);
     return ele;
 }
 
