006b7b6b3458d1e1fc6713bd652a9162e892d1df
Mikey Nichols <mikenichols410@gmail.com>
2024-04-09 14:19:48 -0400

    hides other functionality temporarily

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,33 @@
-import data from './data/dinosaurs.json' assert { type: 'json'};
-import dinoListItem from "./components/dinoListItem.js";
-import './utils/hideFooter.js';
-import filterDinosaursByName from "./utils/filterDinosaurs.js";
-import calculateDiet from './utils/chartHelpers.js';
-import dinoPie from './components/dinoDietChart.js';
-import mapComponent from './components/map.js';
-import dinoOfTheDay from './components/randoDino.js';
-import "../styles/nav.scss";
+// import data from './data/dinosaurs.json' assert { type: 'json'};
+// import dinoListItem from "./components/dinoListItem.js";
+// import './utils/hideFooter.js';
+// import filterDinosaursByName from "./utils/filterDinosaurs.js";
+// import calculateDiet from './utils/chartHelpers.js';
+// import dinoPie from './components/dinoDietChart.js';
+//import mapComponent from './components/map.js';
+// import dinoOfTheDay from './components/randoDino.js';
+import "../styles/main.scss";
 
-data.forEach((dinosaur) => console.log(dinoListItem(dinosaur))); // loop over the json file and call dinoListItem, passing inthe dinosaur each time resulting in a filled list of dinosaurs on screen
+import renderHeader from './components/header.js';
+// data.forEach((dinosaur) => console.log(dinoListItem(dinosaur))); // loop over the json file and call dinoListItem, passing inthe dinosaur each time resulting in a filled list of dinosaurs on screen
 
-const searchBar = document.getElementById('search-bar')
-const pie = document.getElementById('dietChart')
-const dinoDiet = calculateDiet()
-const dinoOfTheDayContainer = document.getElementById('dino-of-the-day')
+// const searchBar = document.getElementById('search-bar')
+// const pie = document.getElementById('dietChart')
+// const dinoDiet = calculateDiet()
+// const dinoOfTheDayContainer = document.getElementById('dino-of-the-day')
 
-dinoOfTheDay(dinoOfTheDayContainer)
+// dinoOfTheDay(dinoOfTheDayContainer)
 
-searchBar.addEventListener('input', () => {
-    document.getElementById('dino-list').innerHTML = ''
-    filterDinosaursByName(searchBar.value).forEach((dinosaur) => console.log(dinoListItem(dinosaur)));
-})
+// searchBar.addEventListener('input', () => {
+//     document.getElementById('dino-list').innerHTML = ''
+//     filterDinosaursByName(searchBar.value).forEach((dinosaur) => console.log(dinoListItem(dinosaur)));
+// })
 
-dinoPie(pie, {
-    labels: ['carnivorous', 'herbivorous', 'omnivorous'],
-    data: dinoDiet
-})
+// dinoPie(pie, {
+//     labels: ['carnivorous', 'herbivorous', 'omnivorous'],
+//     data: dinoDiet
+// })
 
-mapComponent(data)
+//mapComponent(data)
 
+renderHeader();
