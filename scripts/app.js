8a816e6e40750c9a1a4bbbaa1b9c0ef22722d607
Mikey Nichols <mikenichols410@gmail.com>
2024-04-10 18:31:16 -0400

    impoers dinoPie and calculateDiet into app and then uses them, also changes renderMain to await which was incredibly helpful

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,13 @@
 import renderMain from "./components/mainComponent.js";
 import renderHeader from "./components/header/header.component.js";
+import dinoPie from "./components/chart/dinoDietChart.component.js";
+import calculateDiet from "./utils/chartHelpers.js";
 import "../styles/main.scss";
 
-
-
-
+const dinoDiet = calculateDiet();
 renderHeader();
-renderMain();
+await renderMain();
+dinoPie(document.querySelector('main'), {
+  labels: ["carnivorous", "herbivorous", "omnivorous"],
+  data: dinoDiet,
+});
