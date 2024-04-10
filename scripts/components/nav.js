7314f9d8250f25b183759ebec787c4e27ac0c5ea
Mikey Nichols <mikenichols410@gmail.com>
2024-04-09 20:57:04 -0400

    changes logic to make navbar go away when scrolled up

--- scripts/components/nav.js
+++ scripts/components/nav.js
@@ -1,6 +1,33 @@
 import createEle from "../utils/createEle";
+const hideNav = () => {
+    // listens for the user to scroll up or down
+    const getNav = () => document.querySelector("nav");
+    window.onscroll = () => {
+      const nav = getNav();
+      const pageTop = 0; // declares the pageTop to be 0
+        windowYOffset = window.pageYOffset; // declares the pageTop to be the value of the offsetHeight of the body subtracted by the inner height of the window
+      if (pageTop == windowYOffset) nav.classList.remove("shift-up");
+      // if we scroll to the bottom of the page, remove the class-list of shift-down from our footer - rendering it on screen
+      else nav.classList.add("shift-up"); // if we scroll away, hide it again by adding that class back
+    };
+  
+    // https://stackoverflow.com/users/10703934/kia-abdi && https://techstacker.com/javascript-detect-when-scrolled-to-bottom/
+    window.onwheel = (e) => {
+      const footer = getFooter(); // defines footer by running function to find it
+      if (
+        e.deltaY >= 0 &&
+        window.innerHeight + window.pageYOffset >= document.body.offsetHeight
+      ) {
+        // Scrolling down causes the footer to slide up from the bottom
+        footer.classList.remove("shift-up");
+      } else {
+        // Hides the footer when the user scrolls up
+        footer.classList.add("shift-up");
+      }
+    };
+  };
 const content = `
-<input type="checkbox" class="navigation__checkbox" id="navi-toggle">
+<input type="checkbox" class="navigation__checkbox shift-up" id="navi-toggle">
 <label for="navi-toggle" class="navigation__button">
   <span class="navigation__icon">&nbsp;</span>
 </label>
