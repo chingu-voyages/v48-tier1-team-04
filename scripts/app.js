517d7141ad69e7da1b88e3d64a40c7c533cce7a1
Mikey Nichols <mikenichols410@gmail.com>
2024-04-10 17:47:59 -0400

    refactors app to only do what it does

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,9 @@
-// import data from './data/dinosaurs.json' assert { type: 'json'};
-// import dinoListItem from "./components/dinoListItem.js";
-import renderFooter from "./components/footer/footer.component.js";
-// import './utils/hideFooter.js';
-// import filterDinosaursByName from "./utils/filterDinosaurs.js";
-import calculateDiet from "./utils/chartHelpers.js";
-import dinoPie from "./components/dinoDietChart.js";
-//import mapComponent from './components/map.js';
-import dinoOfTheDay from "./components/randoDino.js";
 import renderMain from "./components/mainComponent.js";
+import renderHeader from "./components/header/header.component.js";
 import "../styles/main.scss";
 
-import renderHeader from "./components/header/header.component.js";
-//data.forEach((dinosaur) => console.log(dinoListItem(dinosaur))); // loop over the json file and call dinoListItem, passing inthe dinosaur each time resulting in a filled list of dinosaurs on screen
 
-//mapComponent(data)
+
 
 renderHeader();
 renderMain();
