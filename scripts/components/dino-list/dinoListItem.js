30c1aa71a4f06e5e56d62a2de3943e34cffb9e78
Mikey Nichols <mikenichols410@gmail.com>
2024-04-09 23:42:47 -0400

    adds a random hue to each dinosaur

--- scripts/components/dino-list/dinoListItem.js
+++ scripts/components/dino-list/dinoListItem.js
@@ -32,9 +32,11 @@ const dinoListItem = (dinosaur) => {
   const randomColor = () => `${Math.floor(Math.random()*16777215).toString(16)}6f`; // generates a random color for the background of the dinosaur card
   const parentContainer = document.getElementById('dino-list'); // declares the parent container which is the list of dinosaurs
   parentContainer.style.background = `url(./assets/watercolor/${Math.floor(Math.random()*58)}.png) fixed`; // sets the background of the parent container to the image of the dinosaur
+  parentContainer.style.backgroundSize = 'cover'; // sets the background size of the parent container to cover
   const dinoLi = createEle("li", content, parentContainer, 'dino'); // declares a variable to a new `<li>` with the content of `content` and appended  to the parentContainer
-  //dinoLi.style.background = randomColor(); // sets the background of the list item to the image of the dinosaur
- // dinoLi.onclick = () => displayDinosaur(dinosaur); // adds event listen on click to the list item just created, and calls the `displayDinosaur`, passing in this dinosaur into the function
+  dinoLi.onhover = () => dinoLi.style.backgroundColor = `red`; // adds an event listener to the list item to change the background color on hover
+  dinoLi.style.filter = `hue-rotate(${Math.floor(Math.random()*360)}deg)`; // sets the filter of the list item to a random hue rotation
+  dinoLi.onclick = () => displayDinosaur(dinosaur); // adds event listen on click to the list item just created, and calls the `displayDinosaur`, passing in this dinosaur into the function
   return "Dinosaur added of " + name + " to the list"; // returns a log if we wanted to console.log our results
 };
 
