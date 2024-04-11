71f756b328a6cf192223ed7a12376769496b4d5a
Mikey Nichols <mikenichols410@gmail.com>
2024-04-10 20:14:09 -0400

    reformats code

--- scripts/components/header/nav/nav.component.js
+++ scripts/components/header/nav/nav.component.js
@@ -27,14 +27,32 @@ const content = `
 `;
 let scrollTop = 0;
 
-const toggleDisplay = ele => ele.style.display === "none" ? ele.style.display = "block" : setTimeout(() => ele.style.display = "none", 500); // toggle the display of the navigation items
+const toggleDisplay = (ele) =>
+  ele.style.display === "none"
+    ? (ele.style.display = "block")
+    : setTimeout(() => (ele.style.display = "none"), 500); // toggle the display of the navigation items after 500ms to allow the animation to complete
 const renderNav = () => {
-  const nav = createEle("div", content, document.body, "navigation fade-out", "nav"); // create the navigation element and appends it to document.body
-  const navItems = document.querySelector('.navigation__nav')
-  document.querySelector('nav').style.display = "none";
+  const nav = createEle(
+    "div",
+    content,
+    document.body,
+    "navigation fade-out",
+    "nav"
+  ); // create the navigation element and appends it to document.body
+  const navItems = document.querySelector(".navigation__nav");
+  nav.querySelectorAll("li").forEach(
+    (li) =>
+      (li.onclick = () => {
+        document.querySelector(".navigation__checkbox").checked = false;
+        toggleDisplay(navItems);
+      })
+  ); // close the navigation when a link is clicked
+  
+  document.querySelector("nav").style.display = "none";
   const toTopButton = document.getElementById("toTop");
   toTopButton.onclick = () => window.scrollTo({ top: 0 }); // scroll to the top of the page when the button is clicked
-  document.querySelector('.navigation__button').onclick = () => toggleDisplay(navItems); // toggle the display of the navigation items when the button is clicked
+  document.querySelector(".navigation__button").onclick = () =>
+    toggleDisplay(navItems); // toggle the display of the navigation items when the button is clicked
   window.addEventListener("scroll", () => {
     // get the current scroll position
     let st = window.scrollY || document.documentElement.scrollTop;
@@ -54,12 +72,12 @@ const renderNav = () => {
     }
     if (window.scrollY > 0) {
       // user has scrolled down
-      nav.classList.remove("fade-out"); 
-      nav.classList.add("fade-in"); 
+      nav.classList.remove("fade-out");
+      nav.classList.add("fade-in");
     } else {
       // user is at the top of the page
-      nav.classList.remove("fade-in"); 
-      nav.classList.add("fade-out"); 
+      nav.classList.remove("fade-in");
+      nav.classList.add("fade-out");
     }
     scrollTop = st <= 0 ? 0 : st; // set the scrollTop to 0 if the user is at the top of the page
   });
