2615b8ccd065a60ca087ef3fc71e38f1c84761a2
Mikey Nichols <mikenichols410@gmail.com>
2024-04-12 01:11:30 -0400

    removes renderMain

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,24 @@
-import renderMain from "./components/mainComponent.js";
+import createEle from "./utils/createEle.js";
 import renderBodyMap from "./components/body-map/bodyMap.component.js";
 import renderHeader from "./components/header/header.component.js";
-import dinoOfTheDay from "./components/randoDino/randoDino.component";
+import dinoOfTheDay from "./components/dino-of-the-day/dino-of-the-day.component.js";
 import renderShowcase from "./components/showcase-section/showcase-section.component.js";
 import renderCharts from "./components/chart/dinoDietChart.component.js";
 import renderFooter from "./components/footer/footer.component.js";
 import renderIntroSection from "./components/random-dino-section/intro-section.component.js";
+import renderAllDinos from "./components/renderAllDinos/renderAllDinos.js";
 import "../styles/main.scss";
 
 renderHeader();
-await renderMain();
+createEle('main', '', document.body);
+createEle('div', '', document.body, null, 'dinosaur-modal');
+
 renderShowcase();
 dinoOfTheDay();
 renderBodyMap();
 renderIntroSection();
 renderCharts();
-
-
+renderAllDinos();
 // dinoPie(document.querySelector('main'), {
 //   labels: ["carnivorous", "herbivorous", "omnivorous"],
 //   data: dinoDiet,
