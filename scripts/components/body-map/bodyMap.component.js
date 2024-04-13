abd1211fea5deb9121987aca5bae09b3b4e67197
Mikey Nichols <mikenichols410@gmail.com>
2024-04-13 02:06:05 -0400

    passes in entire dinosaur to placeMarker

--- scripts/components/body-map/bodyMap.component.js
+++ scripts/components/body-map/bodyMap.component.js
@@ -28,7 +28,7 @@ const renderBodyMap = () => {
   });
   const placeMarkers = (map) =>
   dinosaurs.forEach(async (dinosaur) =>
-    placeMarker(map, await getCoords(dinosaur.foundIn), null, dinosaur.name)
+    placeMarker(map, await getCoords(dinosaur.foundIn), null, dinosaur)
   );
 placeMarkers(map);
 return bodyMap;
