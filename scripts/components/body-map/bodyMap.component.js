71582a503aec805482c8114ade9faefa4c07ce2a
Mikey Nichols <mikenichols410@gmail.com>
2024-04-13 00:43:10 -0400

    returns the marker div from placemarker function

--- scripts/components/body-map/bodyMap.component.js
+++ scripts/components/body-map/bodyMap.component.js
@@ -5,17 +5,15 @@ import dinosaurs from "../../data/dinosaurs";
 import createEle from "../../utils/createEle";
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOXAPIKEY;
 
-
-
-
 const renderBodyMap = () => {
   let map;
   const bodyMap = createEle(
     "section",
-    '',
+    "",
     document.querySelector("main"),
     "section-features",
-    "body-map");
+    "body-map"
+  );
   map = new mapboxgl.Map({
     container: "body-map",
     style: "mapbox://styles/mnix-dev/cluiiopsk01ca01ql97063j4f",
@@ -24,15 +22,15 @@ const renderBodyMap = () => {
     pitch: 0,
     bearing: 0,
     projection: "naturalEarth",
-    interactive: false,
   });
   const placeMarkers = (map) =>
-  dinosaurs.forEach(async (dinosaur) =>
-    placeMarker(map, await getCoords(dinosaur.foundIn), null, dinosaur.name)
-  );
-placeMarkers(map);
-return bodyMap;
+    dinosaurs.forEach(async (dinosaur) =>
+      placeMarker(map, await getCoords(dinosaur.foundIn), null, dinosaur.name)
+    );
+  map.dragRotate.disable();
+  map.touchZoomRotate.disableRotation();
+  placeMarkers(map);
+  return bodyMap;
 };
-
 
 export default renderBodyMap;
