5331d73cdcd0b25b6d26bce5de415d975fdb251b
Mikey Nichols <mikenichols410@gmail.com>
2024-04-13 21:54:44 -0400

    adds displayteamLocations from previous versions

--- scripts/utils/mapBox.js
+++ scripts/utils/mapBox.js
@@ -7,21 +7,42 @@ const getCoords = async (query) => {
     .then((response) => response.json())
     .then((data) => data.features[0].geometry.coordinates);
 };
+const displayTeamLocations = (map, image) => {
+const teamLocations = [
+  "Bolivia",
+  "Cincinnati",
+  "Gormania",
+  "Pittsburgh",
+  "Santiago",
+  "Toronto",
+];
+const markers = [];
+teamLocations.forEach(async (location) => {
+  const coordinates = await getCoords(location);
+  markers.push(placeMarker(map, coordinates, image));
+});
+markers.forEach((marker, i) => marker.onclick = () => {
+  const developers = ["sebastianlq", "Supatki", "mnichols08", "JOSIK95", "mikelovin23", "Vinccodes"];
+  const github = dev => `https://github.com/${dev}`;
+  marker.style.cursor = "pointer";
+  marker.onclick = () => window.open(github(developers[i]), '_blank')
+})
+return markers;
+};
 
 const placeMarker = (map, coordinates, image, dinosaur) => {
     const div = document.createElement("div");
-    div.id = `marker-${dinosaur.id}`;
+    if (dinosaur) div.id = `marker-${dinosaur.id}`;
     div.style.width = "50px";
     div.style.height = "50px";
     div.style.backgroundSize = "contain";
+    div.style.cursor = "pointer";
     if (image) div.style.backgroundImage = image;
-    else { 
-      div.style.backgroundImage = `url(./assets/dino-icon-1.png)`;
-      div.style.cursor = "pointer";
-      div.onclick = () => console.log(dinosaur)
-    }
+    else div.style.backgroundImage = `url(./assets/dino-icon-1.png)`;
+      
+    
     new mapboxgl.Marker(div).setLngLat(coordinates).addTo(map);
     return div;
   };
 
-export { getCoords, placeMarker };
+export { getCoords, placeMarker, displayTeamLocations };
